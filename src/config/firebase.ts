dbb45518d7e8574709b6ef9cca33f869bd2feac1
ShahzaibJak <101657949+ShahzaibJak@users.noreply.github.com>
2023-08-21 15:45:46 -0700

    Update firebase.ts
    Add details for a firebase config object

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore'
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAgZMBhDilF5oxrYiYwGBAe3PEM-t9WS4Q",
-  authDomain: "react-social-media-app-a036d.firebaseapp.com",
-  projectId: "react-social-media-app-a036d",
-  storageBucket: "react-social-media-app-a036d.appspot.com",
-  messagingSenderId: "1030572471859",
-  appId: "1:1030572471859:web:8f6a43e5004c3dbd899546"
-};
+const firebaseConfig = {}; //Add your Firebase Config here
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
