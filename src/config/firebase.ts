8ba0f2dad3c5f75e37aac7d5f055e907205500f0
ShahzaibTii <shahzaib@tii-usa.com>
2023-11-19 16:54:11 +0500

    firebase config env added

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,9 +1,15 @@
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from 'firebase/auth';
-import {getFirestore} from 'firebase/firestore'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
-const firebaseConfig = {}; //Add your Firebase Config here
-
+const firebaseConfig = {
+    apiKey: process.env.React_APP_apiKey,
+    authDomain: process.env.React_APP_authDomain,
+    projectId: process.env.React_APP_projectId,
+    storageBucket: process.env.React_APP_storageBucket,
+    messagingSenderId: process.env.React_APP_messagingSenderId,
+    appId: process.env.React_APP_appId,
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
